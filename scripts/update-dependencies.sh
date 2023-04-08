04eada931fb0131f5b1892da0192c40dd6e24ec0
Tanguy Michardière <54041658+tanguyMichardiere@users.noreply.github.com>
2023-04-08 17:47:44 +0200

    fix dependencies

--- scripts/update-dependencies.sh
+++ scripts/update-dependencies.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
-npx -y npm-check-updates -t minor -u
-npx -y npm-check-updates -f !@types/node -u
+npx -y npm-check-updates -x @next/bundle-analyzer -x eslint-config-next -x next -t minor -u
+npx -y npm-check-updates -x @types/node -x @next/bundle-analyzer -x eslint-config-next -x next -u
 npm install
 npm update
