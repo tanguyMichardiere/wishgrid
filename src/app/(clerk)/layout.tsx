bd59ce8d1820105986b42e17a3892dec1b8da00a
Tanguy Michardière <54041658+tanguyMichardiere@users.noreply.github.com>
2023-09-02 17:45:11 +0200

    move return link to only manage account page

--- src/app/(clerk)/layout.tsx
+++ src/app/(clerk)/layout.tsx
@@ -1,4 +1,3 @@
-import Link from "next/link";
 import type { ReactNode } from "react";
 
 export const runtime = "edge";
@@ -9,11 +8,8 @@ type Props = {
 
 export default function ClerkLayout(props: Props): JSX.Element {
   return (
-    <div className="flex flex-col items-center justify-center gap-10 pt-[clamp(2rem,10vw,5rem)]">
-      {props.children}
-      <Link className="link mb-10" href="/settings">
-        Return to settings
-      </Link>
+    <div className="flex justify-center pt-[clamp(2rem,10vw,5rem)]">
+      <div className="mb-20">{props.children}</div>
     </div>
   );
 }
