d58f4709aa75301d45a04abb852457512fd67793
Tanguy Michardière <54041658+tanguyMichardiere@users.noreply.github.com>
2023-09-11 11:18:50 +0200

    trpc on edge

--- src/server/index.ts
+++ src/server/index.ts
@@ -9,12 +9,12 @@ const t = initTRPC.context<Context>().create({ transformer: SuperJSON });
 export const createRouter = t.router;
 export const createMiddleware = t.middleware;
 
-const loggerMiddleware = createMiddleware(async function ({ ctx, path, input, next }) {
-  ctx.log.debug(path, { input });
-  const result = await next({ ctx: { ...ctx, log: ctx.log.with({ path, input }) } });
+const loggerMiddleware = createMiddleware(async function ({ ctx, path, rawInput, next }) {
+  ctx.log.debug(path, { input: rawInput });
+  const result = await next({ ctx: { ...ctx, log: ctx.log.with({ path, input: rawInput }) } });
   const now = Date.now();
   ctx.log.debug(path, {
-    input,
+    input: rawInput,
     timings: {
       createContext: ctx.contextCreated - ctx.start,
       procedure: now - ctx.contextCreated,
