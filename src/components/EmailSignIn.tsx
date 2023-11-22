98f00fe235b00b98e6e07b3a044be0ac27c20f0f
Tanguy Michardière <54041658+tanguyMichardiere@users.noreply.github.com>
2023-11-22 12:23:12 +0100

    add log

--- src/components/EmailSignIn.tsx
+++ src/components/EmailSignIn.tsx
@@ -2,7 +2,7 @@ import { useLocale } from "next-intl";
 import { redirect } from "next/navigation";
 import "server-only";
 import { signIn } from "../auth";
-import { VERCEL_URL } from "../env";
+import { logger } from "../server/logger";
 import { useServerTranslations } from "../utils/translations/server";
 
 export default function EmailSignIn(): JSX.Element {
@@ -16,8 +16,9 @@ export default function EmailSignIn(): JSX.Element {
     const url = (await signIn("email", {
       email: formData.get("email"),
       redirect: false,
-      redirectTo: `${VERCEL_URL}/${locale !== "en" ? `${locale}` : ""}`,
+      redirectTo: `/${locale !== "en" ? `${locale}` : ""}`,
     })) as string;
+    logger.warn(url);
     // TODO: wait for fix in next-auth
     redirect(url.replace("//verify-request", "/api/auth/verify-request"));
   }
