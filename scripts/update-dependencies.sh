772c478dcbfc961f9bab0d8690fa9ec178b1932e
Tanguy Michardière <54041658+tanguyMichardiere@users.noreply.github.com>
2023-11-26 19:11:30 +0100

    use pnpm

--- scripts/update-dependencies.sh
+++ scripts/update-dependencies.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 ONLY_MINOR="@types/node,@tanstack/react-query,@tanstack/react-query-devtools"
-npx npm-check-updates -f $ONLY_MINOR -t minor -u
-npx npm-check-updates -x $ONLY_MINOR -u
-npm install
-npm update
+pnpx npm-check-updates -f $ONLY_MINOR -t minor -u
+pnpx npm-check-updates -x $ONLY_MINOR -u
+pnpm install
+pnpm update
