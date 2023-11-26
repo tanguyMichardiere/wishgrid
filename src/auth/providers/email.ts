25d7707c830ab04979e38c5bf7227249b9e0cef2
Tanguy Michardière <54041658+tanguyMichardiere@users.noreply.github.com>
2023-11-26 19:33:24 +0100

    fix emails

--- src/auth/providers/email.ts
+++ src/auth/providers/email.ts
@@ -1,14 +1,17 @@
 import Email from "next-auth/providers/email";
 import { AUTH_EMAIL_FROM, AUTH_EMAIL_SERVER } from "../../env";
 
-const Provider = Email({ server: AUTH_EMAIL_SERVER, from: AUTH_EMAIL_FROM });
+const Inner = Email({ server: AUTH_EMAIL_SERVER, from: AUTH_EMAIL_FROM });
 
-Provider.sendVerificationRequest = async function sendVerificationRequest({ url, ...params }) {
-  return Provider.sendVerificationRequest({
-    ...params,
-    // TODO: wait for fix in next-auth
-    url: url.replace("//callback/", "/api/auth/callback/"),
-  });
+const Outer: typeof Inner = {
+  ...Inner,
+  async sendVerificationRequest({ url, ...params }) {
+    return Inner.sendVerificationRequest({
+      ...params,
+      // TODO: wait for fix in next-auth
+      url: url.replace("//callback/", "/api/auth/callback/"),
+    });
+  },
 };
 
-export default Provider;
+export default Outer;
