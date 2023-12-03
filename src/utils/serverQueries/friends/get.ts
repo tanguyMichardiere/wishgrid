6fb917d242ac7d9538f077c6ff8e2b9c7abb05fc
Tanguy Michardière <54041658+tanguyMichardiere@users.noreply.github.com>
2023-12-03 19:17:05 +0100

    fix export

--- src/utils/serverQueries/friends/get.ts
+++ src/utils/serverQueries/friends/get.ts
@@ -2,6 +2,6 @@ import { notFound } from "next/navigation";
 import "server-only";
 import { serverQuery } from "..";
 
-export const g = serverQuery((trpc, userId: string) => trpc.friends.get.fetch({ userId }), {
+export const getFriend = serverQuery((trpc, userId: string) => trpc.friends.get.fetch({ userId }), {
   NOT_FOUND: notFound,
 });
