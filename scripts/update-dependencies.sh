73de441d4b9f1cd3cf1d6bd0023d205fe64c0352
Tanguy Michardière <54041658+tanguyMichardiere@users.noreply.github.com>
2023-12-03 19:36:35 +0100

    use sharp 0.32.6

--- scripts/update-dependencies.sh
+++ scripts/update-dependencies.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env sh
-EXCLUDE="@types/node"
+
+# TODO: remove sharp once it works
+EXCLUDE="@types/node,sharp"
 ONLY_MINOR="@tanstack/react-query,@tanstack/react-query-devtools"
+
 pnpm exec npm-check-updates -f "$ONLY_MINOR" -t minor -u
 pnpm exec npm-check-updates -x "$EXCLUDE,$ONLY_MINOR" -u
 pnpm install
