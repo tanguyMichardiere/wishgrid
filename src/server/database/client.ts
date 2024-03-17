0de67130b0816d218287411079a87f45056713aa
Tanguy Michardière <54041658+tanguyMichardiere@users.noreply.github.com>
2024-03-17 14:57:57 +0100

    remove neon serverless

--- src/server/database/client.ts
+++ src/server/database/client.ts
@@ -1,20 +1,8 @@
-import { Pool, neonConfig } from "@neondatabase/serverless";
-import { PrismaNeon } from "@prisma/adapter-neon";
 import { PrismaClient } from "@prisma/client";
 import "server-only";
-import ws from "ws";
-import { env } from "../../env";
 import { logger } from "../logger";
 
-if (!("WebSocket" in global)) {
-  neonConfig.webSocketConstructor = ws;
-}
-
-const pool = new Pool({ connectionString: env.DATABASE_URL });
-const adapter = new PrismaNeon(pool);
-
 export const databaseClient = new PrismaClient({
-  adapter,
   log: [
     { emit: "event", level: "query" },
     { emit: "event", level: "info" },
