21117d122003d5d700b430a466e5152f69a04701
Tanguy Michardière <54041658+tanguyMichardiere@users.noreply.github.com>
2024-03-17 15:10:10 +0100

    update dependencies

--- scripts/update-dependencies.sh
+++ scripts/update-dependencies.sh
@@ -3,6 +3,10 @@
 # TODO: remove sharp once it works
 EXCLUDE="@types/node,sharp"
 
+# TODO: remove once next-auth@5 is released
+pnpm install next-auth@beta
+# TODO: remove once trpc@11 is released
+pnpm install @trpc/client@next @trpc/next@next @trpc/react-query@next @trpc/server@next
 pnpm exec npm-check-updates -x "$EXCLUDE" -u
 pnpm install
 pnpm update
