593056a3631818b9600597f16d967a797f2eddf8
Tanguy Michardière <54041658+tanguyMichardiere@users.noreply.github.com>
2024-09-20 19:52:01 +0200

    update node

--- scripts/update-dependencies.sh
+++ scripts/update-dependencies.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env sh
 
+ONLY_MINOR="@types/node"
 # TODO: remove sharp once it works
-EXCLUDE="@types/node,sharp"
+EXCLUDE="sharp"
 
 # TODO: remove once next-auth@5 is released
 pnpm install next-auth@beta
 # TODO: remove once trpc@11 is released
 pnpm install @trpc/client@next @trpc/next@next @trpc/react-query@next @trpc/server@next
-pnpm exec npm-check-updates -x "$EXCLUDE" -u
+pnpm exec npm-check-updates -f "$ONLY_MINOR" -t minor -u
+pnpm exec npm-check-updates -x "$ONLY_MINOR,$EXCLUDE" -u
 pnpm install
 pnpm update
 biome format --write package.json
