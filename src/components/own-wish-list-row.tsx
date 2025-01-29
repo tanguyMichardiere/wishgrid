bb696fa1ee18aead371e2f1929d0336ac34015b9
Tanguy Michardière <54041658+tanguyMichardiere@users.noreply.github.com>
2025-01-29 17:49:29 +0100

    use status

--- src/components/own-wish-list-row.tsx
+++ src/components/own-wish-list-row.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { EyeIcon } from "@heroicons/react/24/outline";
 import type { JSX } from "react";
 import { useRef } from "react";
 import type { OwnWish } from "../server/database/types/wishes";
@@ -17,9 +16,12 @@ export function OwnWishListRow(props: Props): JSX.Element {
 
 	return (
 		<>
-			<div className="flex list-col-grow items-center">{props.wish.title}</div>
-			<button className="btn btn-circle btn-ghost" onClick={showModal} type="button">
-				<EyeIcon className="h-6 w-6" />
+			<button
+				className="cursor-pointer list-row transition-colors hover:bg-base-200"
+				onClick={showModal}
+				type="button"
+			>
+				<div className="flex list-col-grow items-center">{props.wish.title}</div>
 			</button>
 			<OwnWishModal ref={modalRef} wish={props.wish} />
 		</>
